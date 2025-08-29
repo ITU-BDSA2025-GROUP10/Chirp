ba40714f9a5cdef47fb0a64ff9f1713d8fdc3636
JacobHorberg <jhmaildk@gmail.com>
2025-08-29 11:14:20 +0200

    add streamreader to load csv file

--- Program.cs
+++ Program.cs
@@ -1,7 +1,27 @@
-﻿List<string> cheeps = new () { "hello, BDSA students!", "Welcome to the course", "i hope you had a good summer"};
-
-foreach (var cheep in cheeps)
+﻿using System.Text.RegularExpressions;
+using (StreamReader reader = new StreamReader(@"Data/chirp_cli_db.csv"))
 {
-    Console.WriteLine(cheep);
-    Thread.Sleep(1000);
+    string line; 
+    List<cheep> cheepList = new List<cheep>();
+    while ((line = reader.ReadLine()) != null)
+    {
+        //Define pattern
+        Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+
+        //Separating columns to array
+        string[] lines = CSVParser.Split(line);
+        Console.WriteLine(lines.Length);
+        /*for  (int i = 3; i < lines.Length - 3; i += 3) 
+        {
+           cheep post = new cheep(lines[i], lines[i + 1], DateTimeOffset.Parse(lines[i + 2])); 
+           cheepList.Add(post);
+        }*/
+    }
+
+    /*foreach (cheep cheep in cheepList)
+    {
+        Console.WriteLine(cheep);
+    }*/
+    
 }
+public record cheep(string Author, string message, DateTimeOffset Timestamp);
