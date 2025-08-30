ba396d1d4e2ebaa7ae59420b1179aa80ba746ca3
JacobHorberg <jhmaildk@gmail.com>
2025-08-30 21:25:52 +0200

    Change file reading from streamreader to textfieldparser

--- Program.cs
+++ Program.cs
@@ -1,27 +1,28 @@
-﻿using System.Text.RegularExpressions;
-using (StreamReader reader = new StreamReader(@"Data/chirp_cli_db.csv"))
+﻿using Microsoft.VisualBasic.FileIO;
+using (TextFieldParser parser = new TextFieldParser(@"Data/chirp_cli_db.csv"))
 {
-    string line; 
-    List<cheep> cheepList = new List<cheep>();
-    while ((line = reader.ReadLine()) != null)
+    parser.TextFieldType = FieldType.Delimited;
+    parser.SetDelimiters(",");
+    List<Cheep> cheeps = new List<Cheep>();
+    List<string> lines = new List<string>();
+    while (!parser.EndOfData)
     {
-        //Define pattern
-        Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
-
-        //Separating columns to array
-        string[] lines = CSVParser.Split(line);
-        Console.WriteLine(lines.Length);
-        /*for  (int i = 3; i < lines.Length - 3; i += 3) 
+        string[] fields = parser.ReadFields();
+        foreach (string field in fields)
         {
-           cheep post = new cheep(lines[i], lines[i + 1], DateTimeOffset.Parse(lines[i + 2])); 
-           cheepList.Add(post);
-        }*/
+            lines.Add(field);
+        }
+    }
+    for (int i = 3; i < lines.Count - 3 + 1; i += 3) {
+        Cheep tempCheep = new Cheep(lines[i], lines[i + 1], lines[i + 2]);
+        cheeps.Add(tempCheep);
     }
 
-    /*foreach (cheep cheep in cheepList)
+    foreach (Cheep cheep in cheeps)
     {
         Console.WriteLine(cheep);
-    }*/
+
+    }
     
 }
-public record cheep(string Author, string message, DateTimeOffset Timestamp);
+public record Cheep(string Author, string message, string timeStamp);
