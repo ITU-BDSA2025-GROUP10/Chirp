cb31c3344657ebcbc7d57097bc16f3c0623416a5
JacobHorberg <jhmaildk@gmail.com>
2025-10-30 12:09:43 +0100

    Add in memory test server - doesnt work yet

---
+++ tests/unitTests/CheepRepositoryUnitTests.cs
@@ -0,0 +1,19 @@
+﻿namespace DefaultNamespace;
+
+public class CheepRepositoryUnitTests
+{
+// Arrange
+using var connection = new SqliteConnection("Filename=:memory:");
+await connection.OpenAsync();
+var builder = new DbContextOptionsBuilder<ChirpContext>().UseSqlite(connection);
+
+using var context = new ChirpContext(builder.Options);
+await context.Database.EnsureCreatedAsync(); // Applies the schema to the database
+
+IMessageRepository repository = new MessageRepository(context);
+
+// Act
+var result = repository.QueryMessages("TestUser");
+...
+    
+}
