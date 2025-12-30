0161c6d5b450798fa682a96c81f651e1eb3af64c
JogvanJacobsen <jogv@itu.dk>
2025-12-30 16:46:33 +0100

    removed unused authentication handler class

--- tests/IntegrationTests/CustomWebApplicationFactory.cs
+++ tests/IntegrationTests/CustomWebApplicationFactory.cs
@@ -42,18 +42,6 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
             var db = scope.ServiceProvider.GetRequiredService<ChatDBContext>();
             db.Database.EnsureCreated();
 
-            // Remove existing authentication services
-            services.RemoveAll<IAuthenticationSchemeProvider>();
-
-            // Add test authentication scheme
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = "TestScheme";
-                options.DefaultChallengeScheme = "TestScheme";
-                options.DefaultScheme = "TestScheme";
-            })
-            .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>("TestScheme", options => { });
-
             // Disable antiforgery token validation for testing
             services.AddMvc(options =>
             {
@@ -69,39 +57,5 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
             _connection.Dispose();
         }
         base.Dispose(disposing);
-    }
-}
-
-// Test authentication handler
-public class TestAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
-{
-    public TestAuthHandler(
-        IOptionsMonitor<AuthenticationSchemeOptions> options,
-        ILoggerFactory logger,
-        UrlEncoder encoder)
-        : base(options, logger, encoder)
-    {
-    }
-    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
-    {
-        // Get test user from request headers
-        var testUser = Context.Request.Headers["X-Test-User"].FirstOrDefault();
-
-        if (string.IsNullOrEmpty(testUser))
-        {
-            // No authentication
-            return Task.FromResult(AuthenticateResult.NoResult());
-        }
-
-        var claims = new[]
-        {
-            new Claim(ClaimTypes.Name, testUser),
-            new Claim(ClaimTypes.NameIdentifier, testUser)
-        };
-        var identity = new ClaimsIdentity(claims, "TestScheme");
-        var principal = new ClaimsPrincipal(identity);
-        var ticket = new AuthenticationTicket(principal, "TestScheme");
-
-        return Task.FromResult(AuthenticateResult.Success(ticket));
     }
 }
